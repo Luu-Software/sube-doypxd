6157628cf24866707c9e64add3250c48d4f6c1fd
doypapuxd <alejognus17@gmail.com>
2026-05-13 14:26:15 -0300

    completar consigna manija
    nota: quite el sistema de variables de estructura, volvi a los ifs asi se pueden evaluar al corrregir

--- main.js
+++ main.js
@@ -12,26 +12,7 @@ boton.addEventListener("click", () => {
   let distanciaViaje = distancia.value;
   let resultado = 0;
 
-  if (transporte.value === "colectivo") {
-    // explico, aca hize una variable de estructura porque no quiero hacer tantos else, voy a usar "for"
-    let tarifasSociales = [
-      {
-        "distanciaMax": 3,
-        "tarifa": 339.18
-      },
-      {
-        "distanciaMax": 6,
-        "tarifa": 376.88
-      },
-      {
-        "distanciaMax": 12,
-        "tarifa": 405.91
-      },
-      {
-        "distanciaMax": 27,
-        "tarifa": 434.97
-      }
-    ];
+  if (transporte.value === "nacional") {
     if (registrada.value === "si") {
       if (distanciaViaje <= 3) {
         resultado = 715.24;
@@ -42,7 +23,7 @@ boton.addEventListener("click", () => {
       } else if (distanciaViaje <= 27) {
         resultado = 917.24;
       } else {
-        tarifa.innerText = "¡Distancia demasiado grande! No tenemos tarifas disponibles.";
+        resultado = 959.71;
       }
     }
     else if (registrada.value === "no") {
@@ -55,18 +36,99 @@ boton.addEventListener("click", () => {
       } else if (distanciaViaje <= 27) {
         resultado = 1458.41;
       } else {
+        resultado = 1525.94;
+      }
+    } else if (registrada.value === "social") {
+      if (distanciaViaje <= 3) {
+        resultado = 339.18;
+      } else if (distanciaViaje <= 6) {
+        resultado = 376.88;
+      } else if (distanciaViaje <= 12) {
+        resultado = 405.91;
+      } else if (distanciaViaje <= 27) {
+        resultado = 434.97;
+      } else {
+        resultado = 431.86;
+      }
+    };
+  } else if (transporte.value === "provincia") {
+    if (registrada.value === "si") {
+      if (distanciaViaje <= 3) {
+        resultado = 968.57;
+      } else if (distanciaViaje <= 6) {
+        resultado = 1089.64;
+      } else if (distanciaViaje <= 12) {
+        resultado = 1210.71;
+      } else if (distanciaViaje <= 27) {
+        resultado = 1452.85;
+      } else {
+        resultado = 1708.07;
+      }
+    }
+    else if (registrada.value === "no") {
+      if (distanciaViaje <= 3) {
+        resultado = 1937.14;
+      } else if (distanciaViaje <= 6) {
+        resultado = 2179.28;
+      } else if (distanciaViaje <= 12) {
+        resultado = 2421.42;
+      } else if (distanciaViaje <= 27) {
+        resultado = 2905.70;
+      } else {
+        resultado = 3416.14;
+      }
+    } else if (registrada.value === "social") {
+      if (distanciaViaje <= 3) {
+        resultado = 435.85;
+      } else if (distanciaViaje <= 6) {
+        resultado = 490.33;
+      } else if (distanciaViaje <= 12) {
+        resultado = 544.81;
+      } else if (distanciaViaje <= 27) {
+        resultado = 653.78;
+      } else {
+        resultado = 768.63;
+      }
+    };
+  } else if (transporte.value === "caba") {
+    if (registrada.value === "si") {
+      if (distanciaViaje <= 3) {
+        resultado = 753.74;
+      } else if (distanciaViaje <= 6) {
+        resultado = 837.52;
+      } else if (distanciaViaje <= 12) {
+        resultado = 902.04;
+      } else if (distanciaViaje <= 27) {
+        resultado = 966.61;
+      } else {
+        tarifa.innerText = "¡Distancia demasiado grande! No tenemos tarifas disponibles.";
+      }
+    }
+    else if (registrada.value === "no") {
+      if (distanciaViaje <= 3) {
+        resultado = 1198.45;
+      } else if (distanciaViaje <= 6) {
+        resultado = 1331.66;
+      } else if (distanciaViaje <= 12) {
+        resultado = 1434.24;
+      } else if (distanciaViaje <= 27) {
+        resultado = 1536.91;
+      } else {
         tarifa.innerText = "¡Distancia demasiado grande! No tenemos tarifas disponibles.";
       }
     } else if (registrada.value === "social") {
-      for (elemento of tarifasSociales) {
-        if (distanciaViaje <= elemento.distanciaMax) {
-          elemento.tarifa = resultado;
-          // detener el loop
-          console.log(elemento);
-          break;
-        }
+      if (distanciaViaje <= 3) {
+        resultado = 339.18;
+      } else if (distanciaViaje <= 6) {
+        resultado = 376.88;
+      } else if (distanciaViaje <= 12) {
+        resultado = 405.91;
+      } else if (distanciaViaje <= 27) {
+        resultado = 434.97;
+      } else {
+        tarifa.innerText = "¡Distancia demasiado grande! No tenemos tarifas disponibles.";
       }
-    };
+    }
   };
 
   if (!(resultado === 0)) {
